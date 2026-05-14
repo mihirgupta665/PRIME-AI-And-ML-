1fda963a6ed79ac9e745a1024add1342e28698c9
Mihir Gupta <mihirgupta665@gmail.com>
2026-05-14 11:21:58 +0530

    Agent Creation

--- 10_Reinforcement_Learning/4_Flappy_Bird_Game_Project/agent.py
+++ 10_Reinforcement_Learning/4_Flappy_Bird_Game_Project/agent.py
@@ -1,18 +1,30 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
-env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=True)
 
-state, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.acion_space. sample()
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device= "cpu"
 
-    # Processing:
-    next_state, reward, terminated, _, info = env.step(action)
 
-    # Checking if the player is still alive
-    if terminated:
-        break
+def run(self, is_training=True, render=False) :
+    
+    env = gym.make("FlappyBird-v0", render_mode= "human" if render else None, use_lidar=False)
 
-env.close
+    state, _ = env.reset()
+
+    while True:
+        # Next action:
+        # (feed the observation to your agent here)
+        action = env.acion_space.sample()
+
+        # Processing:
+        next_state, reward, terminated, _, info = env.step(action)
+
+        # Checking if the player is still alive
+        if terminated:
+            break
+
+    env.close
