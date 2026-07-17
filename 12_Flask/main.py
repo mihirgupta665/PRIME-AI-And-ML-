59aaa1d601117651812cd87265f2aec474fc5214
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-17 19:19:42 +0530

    Understanding of url_for and dynmic url and cutom static files extraction

--- 12_Flask/main.py
+++ 12_Flask/main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+# flask is a framework for server backend
+# flask render html from templates folder and render styling and scripts from static folder
+# to chnange static folder name pass static_foler="name_desired" at the time of application creation
+# In flask static files could be accessed with roothost+hierarchy and if you want to change the static file access then pass static_url_path="assets_new" at the time of applciation generation
 
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, static_folder="assets", static_url_path="/assets_new")
 
 @app.route("/")
 def hello_world():
+    #  static file => dynamically generate the url
+    print(url_for("static", filename="style2.css"))
     return render_template("index.html")
 
 @app.route("/login")
---
+++ 12_Flask/static/app.js
@@ -0,0 +1,1 @@
+console.log("Hello Mihir! From JS!")
