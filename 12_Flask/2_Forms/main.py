b143ebd05bdddfd3e4b649da1bf45c6f608247ac
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-18 10:16:35 +0530

    query handling in python with jinja teplating rendering techniques

--- 12_Flask/2_Forms/main.py
+++ 12_Flask/2_Forms/main.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    # query is sent in url at the end in ?key=value   [key is used to access the value]
+    # query is exracted in flask server as arguments like request.args.get("key_string")
+    query = request.args.get("q")
+    print("Value of q "+query)
+    # defult value to a query could be given which acts as the placeholder to be displayed when that specific query parameter is not specified
+    firstname = request.args.get("name", default="anonymous")
+    # jinja templating is used to send the query and receive them. which are accessible in html file with double curly and specified variable name.
+    # so beside file to be render with commma jinja template variable name with its value is sent as { ,variable=value+value'}
+    # multiple queries could be send with and in the url {   url?key1=value1&key2=value2 } and could be extracted individually
+    return render_template("index.html", query=query, name=firstname)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
