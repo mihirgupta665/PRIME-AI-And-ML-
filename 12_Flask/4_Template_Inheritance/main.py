7848a8545694ffc26945ee348e51cec626444a8b
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-19 01:22:32 +0530

    redirect and flash messages

--- 12_Flask/4_Template_Inheritance/main.py
+++ 12_Flask/4_Template_Inheritance/main.py
@@ -1,15 +1,25 @@
 # each file has __name__ private variable with value __main__ inside the file and when imported or outsie the file the __name__ variabel value changes
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
+app.secret_key = "some secret message or key"
+
+app.secret_key = "some secret message or key"
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    # redirect to another route
+    return redirect(url_for("login"))  # dynamic url generation for login route
+
+@app.route("/login")
+def login():
+    return "<p>login page</p>"
 
 @app.route("/contact")
-def contact():    
+def contact():
+    flash("support timings are from 9 to 5")    
     return render_template("contact.html")
     
 
