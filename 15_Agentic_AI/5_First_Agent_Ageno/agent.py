8b2b37aba4a884f96db9340fff7348ea291a4c51
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-23 02:44:27 +0530

    Restore first Agno agent and update gitignore

---
+++ 15_Agentic_AI/5_First_Agent_Ageno/agent.py
@@ -0,0 +1,27 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from agno.models.google import GeminiInteractions
+
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def build_agent():
+    return Agent(
+        model=Groq(id="qwen/qwen3.6-27b"),
+        # model=GeminiInteractions(id="gemini-3-flash-preview"),
+        tools=[DuckDuckGoTools()],
+        markdown=True,
+        # instructions="You are a helpfull, well experienced and expert travel agent.",
+        instructions="You are a helpfull, expert  agent.",
+        add_datetime_to_context=True,
+    )
+
+agent = build_agent()
+
+# agent.print_response("My budget is 1L INR, should I travel to Goa or Phuket?")
+agent.print_response(
+    "what happend today at delhi and jantar mantar", show_tool_calls=True
+)
