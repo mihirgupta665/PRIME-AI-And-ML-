907d0ed942bc5b7bd08bca9ea4415d19feee2b12
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-23 19:14:36 +0530

    Structuring the folder for agents

--- 15_Agentic_AI/8_Agents_Memory/memory.py
+++ 15_Agentic_AI/8_Agents_Memory/memory.py
@@ -8,15 +8,16 @@ from agno.db.sqlite import SqliteDb
 
 load_dotenv()
 
-db = SqliteDb
-
+db = SqliteDb(db_file="agno.db")
+db.clear_memories()
 
 def build_agent():
     return Agent(
+        db=db, 
         model=Groq(id="qwen/qwen3.6-27b"),
         # model=GeminiInteractions(id="gemini-3.5-flash"),
         markdown=True,
-        # instructions="You are a helpfull, well experienced and expert travel agent.",
+        add_history_to_context=True
     )
 
 
@@ -24,3 +25,4 @@ agent = build_agent()
 
 # agent.print_response("My budget is 1L INR, should I travel to Goa or Phuket?")
 agent.print_response("what happend today at delhi and jantar mantar")
+agent.print_response("was there any casualties reported till now")
