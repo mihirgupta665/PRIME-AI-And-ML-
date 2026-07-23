0ab3a7cf5902e9a32f0a504a85ac2032f1358f60
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-23 18:41:12 +0530

    Structuring the folder for agents

--- 15_Agentic_AI/8_Agents_Memory/memory.py
+++ 15_Agentic_AI/8_Agents_Memory/memory.py
@@ -4,26 +4,23 @@ from agno.models.google import GeminiInteractions
 
 from dotenv import load_dotenv
 
-from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.db.sqlite import SqliteDb
 
 load_dotenv()
+
+db = SqliteDb
 
 
 def build_agent():
     return Agent(
-        # model=Groq(id="qwen/qwen3.6-27b"),
-        model=GeminiInteractions(id="gemini-3.5-flash"),
-        tools=[DuckDuckGoTools()],
+        model=Groq(id="qwen/qwen3.6-27b"),
+        # model=GeminiInteractions(id="gemini-3.5-flash"),
         markdown=True,
         # instructions="You are a helpfull, well experienced and expert travel agent.",
-        instructions="You are a helpfull, expert  agent.",
-        add_datetime_to_context=True,
     )
 
 
 agent = build_agent()
 
 # agent.print_response("My budget is 1L INR, should I travel to Goa or Phuket?")
-agent.print_response(
-    "what happend today at delhi and jantar mantar", show_tool_calls=True
-)
+agent.print_response("what happend today at delhi and jantar mantar")
