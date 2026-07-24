cf3bcd0e14112cdf432e7153fc39023cfdb65c90
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-24 11:57:33 +0530

    First stream lit app initialized with title and tab detials implemented

--- 15_Agentic_AI/9_YouTube_Analyzer/ui.py
+++ 15_Agentic_AI/9_YouTube_Analyzer/ui.py
@@ -1,4 +1,13 @@
 import streamlit as st
 from youtube_analyzer import build_youtube_agent
 
- 
+import pandas as pd
+
+st.set_page_config(page_title="Youtube Video Analyzer", layout="centered")
+
+st.title("🎥 AI Youtube Video Analyzer")
+
+def get_agent():
+    return build_youtube_agent()
+
+agent = get_agent
